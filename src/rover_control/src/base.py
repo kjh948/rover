90ec6f4a482e07badb382e8a49e7b29fb2cc4285
kjh948@gmail.com <kjh948@gmail.com>
2024-06-10 23:52:21 +0300

    added battery and temp nodes

--- src/rover_control/src/base.py
+++ src/rover_control/src/base.py
@@ -3,11 +3,20 @@
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32, Float32
+from sensor_msgs.msg import Imu
+
 import serial
 import struct
 import json
 import time
 from robot import Robot
+
+import subprocess
+
+pub_temperature = rospy.Publisher('/cpu_temperature', Float32, queue_size=10)
+pub_imu = rospy.Publisher('/imu_raw', Imu, queue_size=10)
+pub_battery = rospy.Publisher('/battery', Float32, queue_size=10)
+
 
 def cmd_callback(msg):
     """Extract linear.x and angular.z from Twist msg."""
@@ -30,12 +39,34 @@ def cmd_callback(msg):
     rover_conn.speed_input(left_speed=l*100, right_speed=r*100)
 
 
-    #ina219_info
+    # print(rover_conn.ina219_info())
+    # print(rover_conn.imu_info())
+
+def callback_battery(event):    
+    battery = rover_conn.ina219_info()
+    battery = float(json.loads(battery)['bus_V'])
+    battery = (battery - 9.75)*100/3
+    bat = Float32(battery)
+    
+    pub_battery.publish(bat)
 
 
-def control():    
+def callback_temperature(event):    
+    t = subprocess.run(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE).stdout.decode('utf-8').split("=")[1].split("'C")[0]
+    temp = Float32(float(t))
+    pub_temperature.publish(temp)
+
+def callback_imu(event):
+    imu = Imu
+    imu = rover_conn.imu_info()
+    pass
+
+def node():    
+
     rospy.init_node('base', anonymous=True)    
     rospy.Subscriber("/cmd_vel", Twist, cmd_callback)
+
+    rate = rospy.Rate(20) # 10hz
 
     port_name = rospy.get_param('~port','/dev/ttyUSB0')
 
@@ -44,12 +75,21 @@ def control():
     rover_conn.connect()
     rospy.loginfo("mcu was initialized and ready now")
 
-
-    rospy.spin()    
+    rospy.Timer(rospy.Duration(1), callback_battery)
+    rospy.Timer(rospy.Duration(1), callback_temperature)
+    # rospy.Timer(rospy.Duration(1/20.), callback_imu)
+    rospy.spin()
+    # while not rospy.is_shutdown():
+    #     battery = rover_conn.ina219_info()
+    #     imu = rover_conn.imu_info()
+    #     pub_imu.publish(hello_str)
+    #     pub_battery.publish(hello_str)
+    #     rate.sleep()
+    # #    
     
 if __name__ == '__main__':
 
     try:
-        control()
+        node()
     except KeyboardInterrupt:
         pass
